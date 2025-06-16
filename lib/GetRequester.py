c001c50d44600199e81b3b53478e58a20efc07e2
Joseph <j.mburu.pro@gmail.com>
2025-06-16 19:51:18 +0300

    get

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,15 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        # Get the raw byte response from the server
+        response = requests.get(self.url)
+        return response.content  # returns raw bytes
 
     def load_json(self):
-        pass
+        # Use response content and decode JSON into Python data structures
+        response = requests.get(self.url)
+        return response.json()  # returns Python list or dict
